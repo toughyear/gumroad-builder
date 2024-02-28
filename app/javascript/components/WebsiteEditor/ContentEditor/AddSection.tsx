0b7abe886050d133058cfe35025e8e193695ab19
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2024-02-28 14:59:54 +0530

    feat: allow adding different types of sections

--- app/javascript/components/WebsiteEditor/ContentEditor/AddSection.tsx
+++ app/javascript/components/WebsiteEditor/ContentEditor/AddSection.tsx
@@ -1,7 +1,18 @@
+import React, { useRef } from "react";
 import { PlusCircle } from "lucide-react";
-import React from "react";
-import { Section, SectionType } from "../../../types/website";
+import {
+  FooterSectionData,
+  NavbarSectionData,
+  Section,
+  SectionType,
+} from "../../../types/website";
 import { v4 as uuidv4 } from "uuid";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "../../ui/select";
 
 type AddSectionProps = {
   sectionId: string;
@@ -9,24 +20,62 @@ type AddSectionProps = {
 };
 
 function AddSection({ sectionId, addSection }: AddSectionProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleAddSection = (type: SectionType) => {
+    const newSectionData: Section = {
+      id: uuidv4(),
+      type: type,
+      data: {}, // Default data for each section type
+    };
+
+    // Customize the default data for each section type
+    switch (type) {
+      case SectionType.navbar:
+        newSectionData.data = {
+          heading: "New Navbar",
+          showAvatar: false,
+          captureEmail: true,
+        } as NavbarSectionData;
+        break;
+      case SectionType.footer:
+        newSectionData.data = {
+          text: "New Footer",
+          showPoweredBy: true,
+          showCopyright: true,
+        } as FooterSectionData;
+        break;
+      case SectionType.rich_text:
+        newSectionData.data = {
+          content: "New Rich Text Section",
+        };
+        break;
+      default:
+        break;
+    }
+
+    addSection(sectionId, newSectionData);
+  };
+
   return (
-    <div className='w-full h-full relative'>
-      <div
-        onClick={() =>
-          addSection(sectionId, {
-            id: uuidv4(),
-            type: SectionType.navbar,
-            data: {
-              heading: "New Section",
-              captureEmail: false,
-              showAvatar: false,
-            },
-          })
-        }
-        className='flex items-center z-10 hover:bg-black hover:text-white absolute h-8 w-8 rounded-md cursor-pointer bg-white border border-black aspect-square p-2 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2'
+    <div ref={containerRef} className='w-full h-full relative'>
+      <Select
+        onValueChange={(value) => handleAddSection(value as SectionType)}
+        value='none'
       >
-        <PlusCircle className='h-4 w-4' />
-      </div>
+        <SelectTrigger
+          aria-label='Add section'
+          hideChevron
+          className='flex items-center z-10 hover:bg-black hover:text-white absolute h-8 w-8 rounded-md cursor-pointer bg-white border border-black aspect-square p-2 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2'
+        >
+          <PlusCircle className='h-4 w-4' />
+        </SelectTrigger>
+        <SelectContent className='shadow-md rounded-md bg-white border border-gray-200'>
+          <SelectItem value={SectionType.navbar}>Navbar</SelectItem>
+          <SelectItem value={SectionType.footer}>Footer</SelectItem>
+          <SelectItem value={SectionType.rich_text}>Rich Text</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   );
 }
--- app/javascript/components/ui/Select.tsx
+++ app/javascript/components/ui/Select.tsx
@@ -10,10 +10,15 @@ const SelectGroup = SelectPrimitive.Group;
 
 const SelectValue = SelectPrimitive.Value;
 
+type SelectTriggerProps = {
+  hideChevron?: boolean;
+};
+
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger> &
+    SelectTriggerProps
+>(({ className, children, hideChevron, ...props }, ref) => (
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
@@ -23,9 +28,11 @@ const SelectTrigger = React.forwardRef<
     {...props}
   >
     {children}
-    <SelectPrimitive.Icon asChild>
-      <ChevronDown className='h-4 w-4 opacity-50' />
-    </SelectPrimitive.Icon>
+    {!hideChevron && (
+      <SelectPrimitive.Icon asChild>
+        <ChevronDown className='h-4 w-4 opacity-50' />
+      </SelectPrimitive.Icon>
+    )}
   </SelectPrimitive.Trigger>
 ));
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
