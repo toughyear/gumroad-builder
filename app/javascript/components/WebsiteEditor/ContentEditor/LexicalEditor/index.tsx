5215d829d72eca475f9bb62b4acb8febd0757789
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2024-03-01 04:23:42 +0530

    fix: add code styling

--- app/javascript/components/WebsiteEditor/ContentEditor/LexicalEditor/index.tsx
+++ app/javascript/components/WebsiteEditor/ContentEditor/LexicalEditor/index.tsx
@@ -24,6 +24,39 @@ type LexicalEditorProps = {
   config: Parameters<typeof LexicalComposer>["0"]["initialConfig"];
 };
 
+const codeHighlightTheme = {
+  atrule: "text-blue-600",
+  attr: "text-blue-600",
+  boolean: "text-orange-600",
+  builtin: "text-green-600",
+  cdata: "text-gray-600",
+  char: "text-green-600",
+  class: "text-pink-600",
+  "class-name": "text-pink-600",
+  comment: "text-gray-600",
+  constant: "text-orange-600",
+  deleted: "text-orange-600",
+  doctype: "text-gray-600",
+  entity: "text-yellow-600",
+  function: "text-pink-600",
+  important: "text-yellow-600",
+  inserted: "text-green-600",
+  keyword: "text-blue-600",
+  namespace: "text-yellow-600",
+  number: "text-orange-600",
+  operator: "text-yellow-600",
+  prolog: "text-gray-600",
+  property: "text-orange-600",
+  punctuation: "text-gray-500",
+  regex: "text-yellow-600",
+  selector: "text-green-600",
+  string: "text-green-600",
+  symbol: "text-orange-600",
+  tag: "text-orange-600",
+  url: "text-yellow-600",
+  variable: "text-yellow-600",
+};
+
 export function LexicalEditor(props: LexicalEditorProps) {
   return (
     <LexicalComposer initialConfig={props.config}>
@@ -76,8 +109,23 @@ export default function Editor() {
               underline: "underline",
               italic: "italic",
               strikethrough: "line-through",
-              underlineStrikethrough: "underlined-line-through",
+              underlineStrikethrough: "underline line-through",
+              code: "bg-slate-100 text-slate-900 rounded p-1",
             },
+            heading: {
+              h1: "text-4xl font-bold",
+              h2: "text-3xl font-bold",
+              h3: "text-2xl font-bold",
+              h4: "text-xl font-bold",
+              h5: "text-lg font-bold",
+              h6: "text-base font-bold",
+            },
+            list: {
+              ul: "list-disc list-inside",
+              ol: "list-decimal list-inside",
+            },
+            quote: "border-l-4 border-slate-500 pl-4 italic",
+            codeHighlight: codeHighlightTheme,
           },
           // Any custom nodes go here
           nodes: [
