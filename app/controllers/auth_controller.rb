6eba764cafd4da8f4fd7885a15cf20b6fd096a2c
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2024-03-02 06:08:35 +0530

    feat: read redirect_uri from env var

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,9 @@
 class AuthController < ApplicationController
   def gumroad
     client_id = ENV['GUMROAD_APPLICATION_ID']
-    redirect_uri = "http://localhost:3000/auth/gumroad/callback"
+    # Use DEPLOYMENT_URL if it exists, otherwise fallback to localhost
+    base_url = ENV['DEPLOYMENT_URL'] || "http://localhost:3000"
+    redirect_uri = "#{base_url}/auth/gumroad/callback"
     scope = "view_profile" # Customize based on the permissions you need
     gumroad_url = "https://gumroad.com/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}&response_type=code&scope=#{scope}"
     redirect_to gumroad_url, allow_other_host: true
@@ -9,12 +11,16 @@ class AuthController < ApplicationController
 
   def gumroad_callback
     code = params[:code]
+    # Use DEPLOYMENT_URL if it exists, otherwise fallback to localhost
+    base_url = ENV['DEPLOYMENT_URL'] || "http://localhost:3000"
+    redirect_uri = "#{base_url}/auth/gumroad/callback"
+
     response = RestClient.post('https://gumroad.com/oauth/token', {
       client_id: ENV['GUMROAD_APPLICATION_ID'],
       client_secret: ENV['GUMROAD_APP_SECRET'],
       code: code,
       grant_type: 'authorization_code',
-      redirect_uri: "http://localhost:3000/auth/gumroad/callback"
+      redirect_uri: redirect_uri
     })
     access_token = JSON.parse(response.body)['access_token']
 
