161aecc93e5b66751e63b1eb7877bd8287effb3a
toughyear <rajivsingh430@gmail.com>
2024-06-16 22:08:21 +0530

    add analytics

--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("G-K7S5QSNRJ7");
 
 document.addEventListener("turbo:load", () => {
   const root = createRoot(
