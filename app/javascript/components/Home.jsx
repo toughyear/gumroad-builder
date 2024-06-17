d02b7b93bd84a06346324733f20580eba0d0fb7e
toughyear <rajivsingh430@gmail.com>
2024-06-17 14:46:00 +0530

    add Producthunt launch

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -17,9 +17,23 @@ const Home = () => {
             Build a website, powered by Gumroad. Sell your e-book, music, or
             art. Or just write a blog. It's free and easy to get started.
           </p>
-          <button className='elevate-brand' onClick={connectToGumroad}>
-            Start Building
-          </button>
+          <div className='flex flex-col space-y-4'>
+            <div className='flex space-x-4'>
+              <button className='elevate-brand' onClick={connectToGumroad}>
+                Start Building
+              </button>
+              <a href="https://github.com/toughyear/gumroad-builder" target="_blank" class="border items-center border-black p-2 inline-flex">
+                  GitHub
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline-block">
+                    <path d="M7 7h10v10"/>
+                    <path d="M7 17 17 7"/>
+                  </svg>
+               </a>
+            </div>
+            <a href="https://www.producthunt.com/posts/website-builder-for-gumroad-products?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-website&#0045;builder&#0045;for&#0045;gumroad&#0045;products" target="_blank">
+              <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=463752&theme=light" alt="Website&#0032;Builder&#0032;for&#0032;Gumroad&#0032;Products - Build&#0032;custom&#0032;websites&#0032;&#0045;&#0032;integrated&#0032;with&#0032;Gumroad&#0032;products | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" />
+            </a>
+          </div>
         </div>
         <div className='w-full md:w-1/2'>
           <img
